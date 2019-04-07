5b43295a104ad2f58ceca10dd35bb6a0713b88b9
sarunaskas <sarunas.kasnauskas@gmail.com>
2019-04-07 18:03:26 +0300

    added 5 ideas

--- topartists.py
+++ topartists.py
@@ -1,3 +1,12 @@
+'''
+Ideas:
+1) Page with Spotify mots lsitened recent artists
+2) Linked in portfolio from LinkedIn API
+3) Map of cafe's in Vilnius and their ratings. are they roasting??
+4) Deploy with docker to AWS
+5) Blog that can be written with markdown?
+'''
+
 #get top artists or tracks https://developer.spotify.com/console/get-current-user-top-artists-and-tracks/
 
 https://api.spotify.com/v1/me/top/{type}
