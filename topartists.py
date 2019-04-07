642a93b1444d25ba4d218cc5eaf0f748dfe79536
sarunaskas <sarunas.kasnauskas@gmail.com>
2019-04-07 17:46:08 +0300

    ideas for new site

---
+++ topartists.py
@@ -0,0 +1,7 @@
+#get top artists or tracks https://developer.spotify.com/console/get-current-user-top-artists-and-tracks/
+
+https://api.spotify.com/v1/me/top/{type}
+
+#https://developer.spotify.com/console/get-current-user-saved-tracks/
+
+https://api.spotify.com/v1/me/tracks 
